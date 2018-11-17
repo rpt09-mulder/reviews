ce33263aa2d587cae63cef50741609e3377119e2
Kent Ogisu <kentogisu@gmail.com>
2018-11-17 16:30:18 -0500

    [Setup] Completed client setup.  Added dependencies/dev dependencies.  Webpack working

---
+++ client/public/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {};
+
+  handleChange = () => {
+    
+  }
+  render() {
+    return (
+      <div>
+        <h1>Firebnb reviews</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
