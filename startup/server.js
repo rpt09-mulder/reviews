06823178002c087a115d6dfdf96eccb61221413e
Kent Ogisu <kentogisu@gmail.com>
2018-12-18 00:57:16 -0500

    [Added] log statement

--- startup/server.js
+++ startup/server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const port = process.env.PORT || 3003;
 const server = app.listen(port, () => {
   console.log(`listening on port ${port}`);
+  console.log('host: ', process.env.RDS_HOST);
 });
 
 module.exports = server;
