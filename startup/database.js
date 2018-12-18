560d15e4949e0488b7cedf8698c8a847cedd37de
Kent Ogisu <kentogisu@gmail.com>
2018-12-18 00:21:42 -0500

    [WIP] completed aws rds setup for postgres

--- startup/database.js
+++ startup/database.js
@@ -1,15 +1,22 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
 //Setting up debugging environment and env variables
 require('dotenv').config();
 const dbDebugger = require('debug')('app:db');
 
+const connection = {
+  user: process.env.RDS_USERNAME,
+  host: process.env.RDS_HOST,
+  database: process.env.RDS_DB_NAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT
+}
 const connectionString = process.env.DB_URL;
 
 // Connecting DB
-const client = new Client(connectionString);
+const pool = new Pool(connection);
 
-client.connect(() => {
-  dbDebugger('connected to db!');
+pool.connect(() => {
+  console.log('connected to db!');
 });
 
-module.exports = client;
+module.exports = pool;
