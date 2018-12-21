e5c6cd310aeee74b18ad3ce0f8f5bca970e0494f
Kent Ogisu <kentogisu@gmail.com>
2018-12-20 23:30:07 -0500

    [Adding] axios request to App component

---
+++ client/public/src/components/App.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+  componentDidMount() {
+    const ebUrl = 'http://firebnb-reviews.8di9c2yryn.us-east-1.elasticbeanstalk.com'; 
+    let id; path;
+    path = window.location.pathname;
+    if (path === path.match(/\//)) {
+      path = '/1';
+    }
+
+    axios.get(`${ebUrl}${path}`)
+      .then(res => res.data)
+      .then(res => {
+        this.setState({
+          data: res.data[0]
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Firebnb reviews</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
