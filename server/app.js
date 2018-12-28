43342bd31c058107b94816ae1062a58f95047d7b
Kent Ogisu <kentogisu@gmail.com>
2018-12-27 23:07:52 -0500

    [WIP] Added npm cors package to server for cors enabling

--- server/app.js
+++ server/app.js
@@ -6,27 +6,25 @@ const db = require('../db');
 const path = require('path');
 const utils = require('../utilities/utils');
 const pool = require('../startup/database');
+const cors = require('cors');
 
 // const aws = require('../services/aws');
 // const startupDebugger = require('debug')('app:startup');
 // const reviews = require('../router');
 
 //Middleware
+app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/../client/dist')));
-// app.use('/:id', express.static(path.join(__dirname, '/../client/dist')));
+app.use('/:id', express.static(path.join(__dirname, '/../client/dist')));
 
 const client = pool.connect(() => {
   console.log('connected to db!');
-  // console.log('pool: ', pool);
 });
-//Routing
-// app.use('/reviews', reviews);
 
 app.get('/reviews/:id', async (req, res) => {
-  // console.log(pool);
   const id = JSON.parse(req.params.id);
   
   try {
